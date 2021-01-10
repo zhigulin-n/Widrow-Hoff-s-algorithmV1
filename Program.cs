eaadcd0139c1fd8c2615c1816d53c8e123e5954a
Nikita Zhigulin <nikita1number@gmail.com>
2021-01-10 21:55:21 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp6
             Stopwatch time = new Stopwatch();
             time.Start();
             int[,] mass = { { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
-            int[] mass_y = { 1, -1, -1, -1 };
+            int[] mass_y = { -1, 1, 1, 1 };
             double[] mass_cnt = new double[4];
 
             double w1 = 1;
@@ -24,19 +24,28 @@ namespace ConsoleApp6
             while (cnt < 30)
             {
                 Console.WriteLine($"Итерация {cnt + 1}");
-                Console.WriteLine($"w1 = {w1}, w2 = {w2}, S = {S}");
+                Console.WriteLine($"w11({cnt}) = {w1},  w21({cnt}) = {w2}, S = {S}");
                 cnt++;
                 double y1 = Math.Round(w1 * mass[0, 0] + w2 * mass[0, 1] - S, 5);
-                double y2 = Math.Round(w1 * mass[1, 0] + w2 * mass[1, 1] - S, 5);
-                double y3 = Math.Round(w1 * mass[2, 0] + w2 * mass[2, 1] - S, 5);
-                double y4 = Math.Round(w1 * mass[3, 0] + w2 * mass[3, 1] - S, 5);
-                Console.WriteLine($"y1 = {y1}, y2 = {y2}, y3 = {y3}, y4 = {y4}");
-
+                Console.WriteLine($"y1 = {w1} * {mass[0, 0]} + {w2} * {mass[0, 1]} - {S} = {y1}");
                 double E1 = Math.Round(0.5 * Math.Pow(y1 - mass_y[0], 2), 5);
+                Console.WriteLine($"E1 = 0.5 * ({y1} - {mass_y[0]})^2 = {E1}");
+
+
+                double y2 = Math.Round(w1 * mass[1, 0] + w2 * mass[1, 1] - S, 5);
+                Console.WriteLine($"y2 = {w1} * {mass[1, 0]} + {w2} * {mass[1, 1]} - {S} = {y2}");
                 double E2 = Math.Round(0.5 * Math.Pow(y2 - mass_y[1], 2), 5);
+                Console.WriteLine($"E2 = 0.5 * ({y2} - {mass_y[1]})^2 = {E2}");
+
+                double y3 = Math.Round(w1 * mass[2, 0] + w2 * mass[2, 1] - S, 5);
+                Console.WriteLine($"y3 = {w1} * {mass[2, 0]} + {w2} * {mass[2, 1]} - {S} = {y3}");
                 double E3 = Math.Round(0.5 * Math.Pow(y3 - mass_y[2], 2), 5);
+                Console.WriteLine($"E3 = 0.5 * ({y3} - {mass_y[2]})^2 = {E3}");
+
+                double y4 = Math.Round(w1 * mass[3, 0] + w2 * mass[3, 1] - S, 5);
+                Console.WriteLine($"y4 = {w1} * {mass[3, 0]} + {w2} * {mass[3, 1]} - {S} = {y4}");
                 double E4 = Math.Round(0.5 * Math.Pow(y4 - mass_y[3], 2), 5);
-                Console.WriteLine($"e1 = {E1}, e2 = {E2}, e3 = {E3}, e4 = {E4}");
+                Console.WriteLine($"E4 = 0.5 * ({y4} - {mass_y[3]})^2 = {E4}");
 
                 mass_cnt[0] = E1;
                 mass_cnt[1] = E2;
@@ -51,7 +60,6 @@ namespace ConsoleApp6
                         number = i;
                     }
                 }
-                Console.WriteLine($"Максимальное E = {schetch}.");
                 schetch = 0;
 
                 if (number == 0)
@@ -80,7 +88,7 @@ namespace ConsoleApp6
                 }
                 sum = Math.Round(E1 + E2 + E3 + E4, 5);
                 number = 0;
-                Console.WriteLine($"Сумма Е = {sum}.");
+                Console.WriteLine($"E = {E1} + {E2} + {E3} + {E4} = {sum}");
                 Console.WriteLine();
             }
             Console.WriteLine($"Затраченное время: {time.Elapsed}");
